2b0526aadc2752582e482c8300c237ab1db96563
Egore <blossom00077@gmail.com>
2024-12-27 01:32:54 +0300

    learned lesson-6 vue.js

--- script.js
+++ script.js
@@ -15,6 +15,58 @@ function service(url) {
     });
 }
 
+
+
+function init() {
+
+    Vue.component('basket', {
+        props: [
+            'item'
+        ],
+
+        template: `
+        <div class="fixed-area">
+            <div class="basket-card">
+                <div class="basket-card__header">
+                    <h1 class="basket-card__header__title">basket card</h1>
+                    <div class="basket-card__header__delete-icon" @click="$emit('close')"></div>
+                </div>
+
+                <div class="basket-card__content">content</div>
+            </div>
+
+        </div>
+        `
+    });
+
+    Vue.component('custom_button', {
+        props: [
+            'item'
+        ],
+
+        template: `
+        <button class="card-button" type="button" v-on:click="$emit('click')">
+        <slot></slot>
+        </button>
+        `
+    });
+
+    Vue.component('goods_item', {
+        props: [
+            'item'
+        ],
+
+        template: `
+        <div class="goods-item">
+         <h3>{{ item.product_name }}</h3>
+         <p>{{ item.price }}</p>
+        </div>
+        `
+    });
+}
+
+
+
 const app = new Vue({
     el: document.getElementById('root'),
 
@@ -53,7 +105,7 @@ const app = new Vue({
 
 
 
-
+window.onload = init;
 
 
 
